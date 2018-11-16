16d4e8f4d2c42f3a8caf89f7154f6963de0134ab
Nick Avramoussis <nna@dneg.com>
2018-11-16 11:59:00 +0000

    Removed incorrect header from openvdb_ax_houdini/SOP_OpenVDB_AX.cc

--- openvdb_ax_houdini/SOP_OpenVDB_AX.cc
+++ openvdb_ax_houdini/SOP_OpenVDB_AX.cc
@@ -46,8 +46,6 @@
 #include <openvdb_ax/compiler/PointExecutable.h>
 #include <openvdb_ax/compiler/VolumeExecutable.h>
 
-#include <openvdb_ax/deprecated/tools.h>
-
 #include <houdini_utils/ParmFactory.h>
 #include <houdini_utils/geometry.h>
 #include <openvdb_houdini/Utils.h>
